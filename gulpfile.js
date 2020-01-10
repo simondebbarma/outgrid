4280afcf33f499071e5c4e366ccc584995756c78
Simon Peter Debbarma <31207418+simonpeterdebbarma@users.noreply.github.com>
2020-01-10 10:33:57 -0600

    :heavy_plus_sign: add gulp-sass, setup the same, add starter files, add gulpfile.js
    the usual! so excited about this simple project. it has potential.

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const del = require('del');
+
+gulp.task('styles', () => {
+    return gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('clean', () => {
+    return del([
+        'css/main.css',
+    ]);
+});
+
+gulp.task('default', gulp.series(['clean', 'styles']));
